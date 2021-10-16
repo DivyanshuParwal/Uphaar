d6ac85dcd9f9bc286c15d8a7a596a96833d4fb62
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2021-10-16 14:25:11 +0530

    update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyDyQhVfmWDvvtzUAmAijCGlc0pSu6igS2o",
-    authDomain: "easygifts-62087.firebaseapp.com",
-    projectId: "easygifts-62087",
-    storageBucket: "easygifts-62087.appspot.com",
-    messagingSenderId: "840663755012",
-    appId: "1:840663755012:web:0bd1c297c95b396f9f8851"
+	apiKey: 'AIzaSyCG9r--1ptpGF6oCC2DIqDcXjYulGQyjSI',
+	authDomain: 'giftasy-65eb4.firebaseapp.com',
+	projectId: 'giftasy-65eb4',
+	storageBucket: 'giftasy-65eb4.appspot.com',
+	messagingSenderId: '938900148745',
+	appId: '1:938900148745:web:b3d21e4f28d5e267c0ded8',
+	measurementId: 'G-T8882W17JT'
 });
 
 const db = firebaseApp.firestore();
